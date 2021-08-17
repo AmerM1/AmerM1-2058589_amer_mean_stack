47e72547f24e2f2c58f2ce86703e497810a18148
Amer mohamed <amohamed20@protonmail.com>
2021-08-17 18:26:17 -0400

    done

---
+++ Phase2/TaskTracker/src/app/model.class.ts
@@ -0,0 +1,10 @@
+export class ToDoList {
+    constructor(
+        public EmployeeName:string,
+        public EmployeeId:number,
+        public task:string,
+        public deadline:string,
+
+        ){
+    }
+}
---
+++ Phase2/TaskTracker/src/app/task-tracker/task-tracker.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ToDoList } from '../model.class';
+import { NgForm } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
+
+@Component({
+  selector: 'app-task-tracker',
+  templateUrl: './task-tracker.component.html',
+  styleUrls: ['./task-tracker.component.css']
+})
+export class TaskTrackerComponent implements OnInit {
+
+  isTableVisible: boolean = false;
+  notVisibil:string="Show to do list";
+  ToDoList:Array<ToDoList>=new Array();
+
+constructor() { }
+
+  ngOnInit(): void {
+  }
+  // addTask(taskRef:NgForm){}
+
+  showTable() {
+  if(this.isTableVisible == false){
+  this.isTableVisible=true
+  this.notVisibil="Hide to do list"
+  }else{
+    this.isTableVisible=false
+    this.notVisibil="Show to do list"
+
+  }
+}
+
+  AddToDo(empId:any,empName:any,empTask:any,empDeadline:any){
+    let Empployeid =empId.value;
+    let EmployeeId =empName.value;
+    let task =empTask.value;
+    let deadline =empDeadline.value;
+
+    let x = new ToDoList(Empployeid,EmployeeId,task,deadline)
+    this.ToDoList.push(x)
+    alert("new task added")
+
+  }
+
+    }
